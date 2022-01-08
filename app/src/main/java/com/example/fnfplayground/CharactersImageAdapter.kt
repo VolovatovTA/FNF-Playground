2f45043ceb15e0462da3a224fa48edc623af3c48
VolovatovTA <tima929_v@rambler.ru>
2022-01-08 10:46:52 +0300

    add list of characters

--- app/src/main/java/com/example/fnfplayground/CharactersImageAdapter.kt
+++ app/src/main/java/com/example/fnfplayground/CharactersImageAdapter.kt
@@ -1,2 +1,39 @@
-package com.example.fnfplayground 
+package com.example.fnfplayground
 
+import android.content.Context
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.FrameLayout
+import android.widget.GridView
+import android.widget.ImageView
+
+class CharactersImageAdapter(private val context: Context) : BaseAdapter() {
+    var arrayListCharactersImage = emptyArray<Int>()
+    init {
+        arrayListCharactersImage = arrayOf(R.drawable.icon1char, R.drawable.icon1char,
+            R.drawable.icon1char, R.drawable.icon1char, R.drawable.icon1char, R.drawable.icon1char )
+    }
+    override fun getCount(): Int {
+        return arrayListCharactersImage.size
+//        return 5
+    }
+
+    override fun getItem(position: Int): Any {
+        return arrayListCharactersImage[position]
+    }
+
+    override fun getItemId(position: Int): Long {
+        return position.toLong()
+    }
+
+    override fun getView(position: Int, p1: View?, p2: ViewGroup?): View {
+        val imageView = ImageView(context)
+        imageView.setImageResource(arrayListCharactersImage[position])
+        imageView.layoutParams = FrameLayout.LayoutParams(85, 85)
+        imageView.scaleType = ImageView.ScaleType.CENTER_CROP
+
+        return imageView
+    }
+
+}
--- app/src/main/java/com/example/fnfplayground/MainActivity.kt
+++ app/src/main/java/com/example/fnfplayground/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.fnfplayground
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.AttributeSet
+import android.view.View
+import android.view.WindowManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateView(name: String, context: Context, attrs: AttributeSet): View? {
+        window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        window.addFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
+        return super.onCreateView(name, context, attrs)
     }
 }
